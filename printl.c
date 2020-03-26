ed6537fa68b273669ba861da89cad2204a39d050
Sailor-rui <2376027488@qq.com>
2020-03-26 18:10:42 +0800

    转换数量与打印数量

---
+++ printl.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int ten =10;
+int two =2;
+printf("Doing it right:");
+printf("%d minus %d is %d\n", ten, two,ten -two);
+printf("%d minus %d is %d\n",ten);
+getchar();
+return 0;
+}
+
