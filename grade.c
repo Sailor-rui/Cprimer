5cb398ed0c1a50e9ab15ab04d131a23a103e9a59
Sailor-rui <2376027488@qq.com>
2020-04-02 00:08:03 +0800

    switch多分支

--- grade.c
+++ grade.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main()
 {
-	//char grade;
-	int n;
-	scanf("%f",&n);
+	//char grade;浮点 
+	int n;//整型 
+	scanf("%d",&n);
 	printf("You score:");
 	switch(n) 
 	{
-		case 1.1:printf("85-100\n");break;
-		case 2.1:printf("7O-84\n");break;
+		//printf("You score:");
+		case 1:printf("85-100\n");break;
+		case 2:printf("7O-84\n");break;
 		//case 'C':printf("60-69\n");break;
 		//case 'D':printf("85-100\n");break;
 		default:printf("error\n");
 	}
 	return 0;
 }
+//case适用于开关语句的 关键字，功能：判断表达式的值然后跳转到与表达式相同的值，对应的case执行。
+//break 执行一条 case后跳出语句作用/可使程序终止循环 
 
